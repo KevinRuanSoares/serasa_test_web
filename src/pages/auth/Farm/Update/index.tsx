9fa5a342fbdc237d3d94f80384278df93aee23fe
Kevin Ruan Soares <soares.ruan.kevin@gmail.com>
2025-01-25 23:55:49 -0300

    wip;

--- src/pages/auth/Farm/Update/index.tsx
+++ src/pages/auth/Farm/Update/index.tsx
@@ -1,4 +1,3 @@
-// src/pages/FarmUpdate/index.tsx
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { IRootState } from "../../../../redux/store";
@@ -49,7 +48,6 @@ const FarmUpdate: React.FC = () => {
   useEffect(() => {
     dispatch(setCurrentPageTitle({ title: "Editar Fazenda" }));
   }, [dispatch]);
-
 
   useEffect(() => {
     const fetchProducers = async () => {
@@ -181,13 +179,40 @@ const FarmUpdate: React.FC = () => {
               <FormRow>
                 <FormField>
                   <FormLabel htmlFor="state">Estado:</FormLabel>
-                  <FormInput
+                  <FormSelect
                     id="state"
-                    type="text"
                     value={state}
                     onChange={(e) => setState(e.target.value)}
-                    placeholder="Digite o estado"
-                  />
+                  >
+                    <option value="">Selecione um estado</option>
+                    <option value="AC">Acre (AC)</option>
+                    <option value="AL">Alagoas (AL)</option>
+                    <option value="AP">Amapá (AP)</option>
+                    <option value="AM">Amazonas (AM)</option>
+                    <option value="BA">Bahia (BA)</option>
+                    <option value="CE">Ceará (CE)</option>
+                    <option value="DF">Distrito Federal (DF)</option>
+                    <option value="ES">Espírito Santo (ES)</option>
+                    <option value="GO">Goiás (GO)</option>
+                    <option value="MA">Maranhão (MA)</option>
+                    <option value="MT">Mato Grosso (MT)</option>
+                    <option value="MS">Mato Grosso do Sul (MS)</option>
+                    <option value="MG">Minas Gerais (MG)</option>
+                    <option value="PA">Pará (PA)</option>
+                    <option value="PB">Paraíba (PB)</option>
+                    <option value="PR">Paraná (PR)</option>
+                    <option value="PE">Pernambuco (PE)</option>
+                    <option value="PI">Piauí (PI)</option>
+                    <option value="RJ">Rio de Janeiro (RJ)</option>
+                    <option value="RN">Rio Grande do Norte (RN)</option>
+                    <option value="RS">Rio Grande do Sul (RS)</option>
+                    <option value="RO">Rondônia (RO)</option>
+                    <option value="RR">Roraima (RR)</option>
+                    <option value="SC">Santa Catarina (SC)</option>
+                    <option value="SP">São Paulo (SP)</option>
+                    <option value="SE">Sergipe (SE)</option>
+                    <option value="TO">Tocantins (TO)</option>
+                  </FormSelect>
                 </FormField>
                 <FormField>
                   <FormLabel htmlFor="totalArea">Área Total (ha):</FormLabel>
